cb8244526147531e0b2302e4ba760027a5d0f9fe
Фаниль <71935758+KutlizamaevOFC@users.noreply.github.com>
2021-07-05 17:57:13 +0300

    Rename id layout elements

--- app/src/main/java/com/dester/summerandroidpractice2021/ThirdScreen.kt
+++ app/src/main/java/com/dester/summerandroidpractice2021/ThirdScreen.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class ThirdScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.ThirdScreen)
+        setContentView(R.layout.third_screen)
     }
 }
