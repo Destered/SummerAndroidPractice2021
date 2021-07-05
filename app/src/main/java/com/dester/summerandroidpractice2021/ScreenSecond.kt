8b1de51b08cebdecb079a9537c83e83cfe9662f8
Daniil <fatikoff2002@mail.ru>
2021-07-05 18:14:22 +0500

    add screen 2

---
+++ app/src/main/java/com/dester/summerandroidpractice2021/ScreenSecond.kt
@@ -0,0 +1,11 @@
+package com.dester.summerandroidpractice2021
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScreenSecond : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen_second)
+    }
+}
