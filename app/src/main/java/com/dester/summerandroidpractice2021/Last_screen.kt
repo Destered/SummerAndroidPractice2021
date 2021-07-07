3bea16bf8d89d868025cf13ce745fbcf1374e482
Hightlighterbabe <victorialatypova22@gmail.com>
2021-07-07 18:50:56 +0300

    screen4

---
+++ app/src/main/java/com/dester/summerandroidpractice2021/Last_screen.kt
@@ -0,0 +1,11 @@
+package com.dester.summerandroidpractice2021
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Last_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_last_screen)
+    }
+}
