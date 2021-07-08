13956ae9be4d820c74bcf84834ddec16eb8d8885
Timur Boltachev <timur.boltachev@joy-dev.com>
2021-07-08 21:54:45 +0300

    Работает, но немного логику подправить нужно

--- app/src/main/java/com/dester/summerandroidpractice2021/MainActivity.kt
+++ app/src/main/java/com/dester/summerandroidpractice2021/MainActivity.kt
@@ -1,25 +1,27 @@
 package com.dester.summerandroidpractice2021
 
 
-import android.content.DialogInterface
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dester.summerandroidpractice2021.databinding.ActivityMainBinding
+import com.whiteelephant.monthpicker.MonthPickerDialog
 import java.text.SimpleDateFormat
 import java.util.*
 
 
-public class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     var myYear = SimpleDateFormat("yyyy").format(Date()).toInt()
     lateinit var binding: ActivityMainBinding
     private val adapter = YearAdapter()
-    private val imageIdList = listOf(R.drawable.photo1,
+    private val imageIdList = listOf(
+        R.drawable.photo1,
         R.drawable.photo2,
         R.drawable.photo3,
-        R.drawable.photo4)
+        R.drawable.photo4
+    )
     private var index = 0
     private var yeartemp = 0
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,22 +31,51 @@ public class MainActivity : AppCompatActivity() {
         init()
     }
 
+    fun showYearPicker() {
+        val calendar: Calendar = Calendar.getInstance()
+        val builder = MonthPickerDialog.Builder(
+            this,
+            object : MonthPickerDialog.OnDateSetListener {
+                override fun onDateSet(selectedMonth: Int, selectedYear: Int) {
 
-    fun init(){
-        binding.apply {
-            rvPhotoYear.layoutManager = LinearLayoutManager(this@MainActivity)
-            rvPhotoYear.adapter = adapter
-            btnAddYear.setOnClickListener{
-                val newList:ArrayList<Year> = adapter.getList()
-                val dialog = DialogYear()
-                if(index>3) index = 0
-                newList.add(Year(imageIdList[index],dialog.yearName.toString()))
-                val diffUtilsCallback = YearDiffUtilsCallback(adapter.getList(),newList)
+                }
+
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH)
+        )
+
+        val newList: ArrayList<Year> = adapter.getList()
+        builder.setMinYear(2000)
+            .setMaxYear(calendar.get(Calendar.YEAR))
+            .showYearOnly()
+            .setOnYearChangedListener { year ->
+                /*if(yearContains(year){
+                Toast.makeText("Год уже занят").show()
+                }else{
+                    newList.add(Year(imageIdList[index],year.toString()))
+                    val diffUtilsCallback = YearDiffUtilsCallback(adapter.getList(),newList)
+                    val resultDiffUtilsCallback = DiffUtil.calculateDiff(diffUtilsCallback)
+                    adapter.setItems(newList)
+                    resultDiffUtilsCallback.dispatchUpdatesTo(adapter)
+                }
+               */
+                newList.add(Year(imageIdList[index], year.toString()))
+                val diffUtilsCallback = YearDiffUtilsCallback(adapter.getList(), newList)
                 val resultDiffUtilsCallback = DiffUtil.calculateDiff(diffUtilsCallback)
                 adapter.setItems(newList)
                 resultDiffUtilsCallback.dispatchUpdatesTo(adapter)
-                index++
-                yeartemp++
+            }.build()
+            .show()
+    }
+
+
+    fun init() {
+        binding.apply {
+            rvPhotoYear.layoutManager = LinearLayoutManager(this@MainActivity)
+            rvPhotoYear.adapter = adapter
+            btnAddYear.setOnClickListener {
+                showYearPicker()
             }
         }
     }
