8ee220065d50876142b6e4e9673c6efbd38b12a1
BIMOS <dart102ruspro@gmail.com>
2021-07-08 17:57:04 +0500

    fix MainScreen

---
+++ app/src/main/java/com/dester/summerandroidpractice2021/DialogYear.kt
@@ -0,0 +1,11 @@
+package com.dester.summerandroidpractice2021
+
+import android.app.Dialog
+import android.app.DialogFragment
+import android.os.Bundle
+
+class DialogYear: DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState)
+    }
+}
