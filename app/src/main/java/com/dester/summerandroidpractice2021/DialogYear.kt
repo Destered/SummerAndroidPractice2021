92b0b28bfbc1632fb203542900aed5021cdcbe5e
BIMOS <dart102ruspro@gmail.com>
2021-07-08 21:42:39 +0500

    add Dialog

--- app/src/main/java/com/dester/summerandroidpractice2021/DialogYear.kt
+++ app/src/main/java/com/dester/summerandroidpractice2021/DialogYear.kt
@@ -1,11 +1,24 @@
 package com.dester.summerandroidpractice2021
 
 import android.app.Dialog
-import android.app.DialogFragment
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
 
-class DialogYear: DialogFragment() {
+class DialogYear : DialogFragment() {
+
+    private val yearNames = arrayOf("2156", "34", "456")
+    public var yearName = Int
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return super.onCreateDialog(savedInstanceState)
+        return activity?.let {
+            val builder = AlertDialog.Builder(it)
+            builder.setTitle("Choose a year")
+                .setItems(yearNames
+                ) { dialog, which ->
+                    yearName
+                }
+            builder.create()
+        } ?: throw IllegalStateException("Activity cannot be null")
     }
 }
--- app/src/main/java/com/dester/summerandroidpractice2021/MainActivity.kt
+++ app/src/main/java/com/dester/summerandroidpractice2021/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.dester.summerandroidpractice2021
 
+
+import android.content.DialogInterface
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.dester.summerandroidpractice2021.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 public class MainActivity : AppCompatActivity() {
-
-
+    var myYear = SimpleDateFormat("yyyy").format(Date()).toInt()
     lateinit var binding: ActivityMainBinding
     private val adapter = YearAdapter()
     private val imageIdList = listOf(R.drawable.photo1,
@@ -22,17 +26,17 @@ public class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         init()
-
-
     }
+
 
     public  fun init(){
         binding.apply {
             rvPhotoYear.layoutManager=LinearLayoutManager(this@MainActivity)
             rvPhotoYear.adapter = adapter
             btnAddYear.setOnClickListener{
+                val dialog = DialogYear()
                 if(index>3) index = 0
-                val year = Year(imageIdList[index],(2021-yeartemp).toString())
+                val year = Year(imageIdList[index],dialog.yearName.toString())
                 adapter.addYear(year)
                 index++
                 yeartemp++
