c0bfbf85620479ba4f83a173b2425a5c51897163
Hightlighterbabe <victorialatypova22@gmail.com>
2021-07-10 14:57:11 +0300

    screen4

--- app/src/main/java/com/dester/summerandroidpractice2021/LastScreen.kt
+++ app/src/main/java/com/dester/summerandroidpractice2021/LastScreen.kt
@@ -1,21 +1,49 @@
 package com.dester.summerandroidpractice2021
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
 import com.dester.summerandroidpractice2021.databinding.ActivityLastScreenBinding
+
 
 class LastScreen : AppCompatActivity() {
 
-    private lateinit var binding:ActivityLastScreenBinding
+    lateinit var bindingClass:ActivityLastScreenBinding
     private lateinit var adapter: DayAdapter
+    private lateinit var button: Button
+    private final val selectPicture = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLastScreenBinding.inflate(layoutInflater)
+        bindingClass = ActivityLastScreenBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+        val img = bindingClass.rvImage.Recycler()
         adapter = DayAdapter()
-        binding.btnAdd.setOnClickListener {
-
+        bindingClass.btnAdd.setOnClickListener {
+           val i: Intent = Intent()
+            intent.setType("image/*")
+            intent.setAction(Intent.ACTION_GET_CONTENT)
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), selectPicture)
         }
-        setContentView(R.layout.activity_last_screen)
+
+    }
+    public fun onActivityResult (int requestCode, int resultCode, Intent data){
+        if(requestCode == resultOk){
+            if(requestCode == selectPicture) {
+                Uri selectedImageUri = data.getData()
+                 val selectedImagePath = getPath(selectedImageUri)
+                bindingClass.rvImage.Recycler() = selecteedImagePath
+                img.setImageURI(selectedImageUri)
+            }
+            }
+        }
+    public fun getPath(uri: Uri){
+
+    }
     }
 }
