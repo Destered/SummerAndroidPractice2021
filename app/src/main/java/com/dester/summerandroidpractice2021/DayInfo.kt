0772f56cfebf0b0714f9a530c3f61007549d5190
Hightlighterbabe <victorialatypova22@gmail.com>
2021-07-11 19:51:24 +0300

    screenLast

--- app/src/main/java/com/dester/summerandroidpractice2021/DayInfo.kt
+++
@@ -1,7 +0,0 @@
-package com.dester.summerandroidpractice2021
-
-import android.graphics.Bitmap
-
-data class DayInfo(
-    val dayImage:Bitmap
-    )
--- app/src/main/java/com/dester/summerandroidpractice2021/LastScreen.kt
+++ app/src/main/java/com/dester/summerandroidpractice2021/LastScreen.kt
@@ -1,49 +1,96 @@
 package com.dester.summerandroidpractice2021
 
-import android.R
+
+import android.Manifest
+import android.content.Context
 import android.content.Intent
-import android.net.Uri
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.checkSelfPermission
+import com.dester.summerandroidpractice2021.data.models.Day
+import com.dester.summerandroidpractice2021.data.models.Events
+import com.dester.summerandroidpractice2021.data.models.Singleton
+import com.dester.summerandroidpractice2021.data.models.Utils
 import com.dester.summerandroidpractice2021.databinding.ActivityLastScreenBinding
+import com.fondesa.kpermissions.allGranted
+import com.fondesa.kpermissions.extension.permissionsBuilder
+import com.fondesa.kpermissions.extension.send
 
 
 class LastScreen : AppCompatActivity() {
 
-    lateinit var bindingClass:ActivityLastScreenBinding
-    private lateinit var adapter: DayAdapter
-    private lateinit var button: Button
-    private final val selectPicture = 1
+    private lateinit var bindingClass:ActivityLastScreenBinding
+    private lateinit var dayInfo: Day
+    private lateinit var pickPhoto: Intent
+    lateinit var database: Events
+    var yearNumber = 0
+    var monthNumber = 0
+    var dayNumber = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        database = Singleton.getInstance(this)
         bindingClass = ActivityLastScreenBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
-        val img = bindingClass.rvImage.Recycler()
-        adapter = DayAdapter()
+        init()
+        bindingClass.btnBack.setOnClickListener{
+            onClickBack()
+        }
         bindingClass.btnAdd.setOnClickListener {
-           val i: Intent = Intent()
-            intent.setType("image/*")
-            intent.setAction(Intent.ACTION_GET_CONTENT)
-            startActivityForResult(Intent.createChooser(intent, "Select Picture"), selectPicture)
+            check(this)
         }
 
     }
-    public fun onActivityResult (int requestCode, int resultCode, Intent data){
-        if(requestCode == resultOk){
-            if(requestCode == selectPicture) {
-                Uri selectedImageUri = data.getData()
-                 val selectedImagePath = getPath(selectedImageUri)
-                bindingClass.rvImage.Recycler() = selecteedImagePath
-                img.setImageURI(selectedImageUri)
-            }
-            }
-        }
-    public fun getPath(uri: Uri){
+    fun init(){
+        yearNumber = intent.getIntExtra("yearNumber", 0)
+        monthNumber = intent.getIntExtra("monthNumber", 0)
+        dayNumber = intent.getIntExtra("dayNumber", 0)
+        val generalDay = database.years[yearNumber].mounths[monthNumber].days[dayNumber]
+        generalDay.imageSourceView?.let { bindingClass.imgv2.setImageDrawable(Utils.stringToImage(it)) }
+
 
     }
+    fun onRequest(){
+
+        permissionsBuilder(Manifest.permission.CAMERA).build().send{
+                result -> if (result.allGranted()){
+                    openGallery()
+                   }
+            }
+        }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            1 -> if (resultCode == RESULT_OK) {
+                val selectedImage = data?.getExtras()?.get("data") as Bitmap
+                bindingClass.imgv2.setImageBitmap(selectedImage)
+
+            }
+        }
+    }
+    fun openGallery(){
+        pickPhoto = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(pickPhoto, 1)
+
+    }
+
+fun check(context: Context){
+    if (checkSelfPermission(context,Manifest.permission.CAMERA)
+        != PackageManager.PERMISSION_GRANTED) {
+        onRequest()
+
+        return;
+
+    }
+    else{
+        openGallery()
+    }
+}
+    fun onClickBack(){
+        this.finish()
     }
 }
