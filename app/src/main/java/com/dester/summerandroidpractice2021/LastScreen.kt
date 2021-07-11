279a7670c6fb17302da0bfa72bdeffab97bc3217
Hightlighterbabe <victorialatypova22@gmail.com>
2021-07-11 23:59:28 +0300

    fullLastScreen

--- app/src/main/java/com/dester/summerandroidpractice2021/LastScreen.kt
+++ app/src/main/java/com/dester/summerandroidpractice2021/LastScreen.kt
@@ -5,7 +5,6 @@ import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.os.Bundle
@@ -32,12 +31,16 @@ class LastScreen : AppCompatActivity() {
     var monthNumber = 0
     var dayNumber = 0
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         database = Singleton.getInstance(this)
         bindingClass = ActivityLastScreenBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
         init()
+        bindingClass.bSaveText.setOnClickListener{
+            database.years[yearNumber].mounths[monthNumber].days[dayNumber].description = bindingClass.inputText.text.toString()
+        }
         bindingClass.btnBack.setOnClickListener {
             onClickBack()
         }
@@ -54,7 +57,7 @@ class LastScreen : AppCompatActivity() {
         dayNumber = intent.getIntExtra("dayNumber", 0)
         dayInfo = database.years[yearNumber].mounths[monthNumber].days[dayNumber]
         dayInfo.imageSourceView?.let { bindingClass.imgv2.setImageDrawable(Utils.stringToImage(it)) }
-
+        bindingClass.inputText.setText(dayInfo.description ?:"")
 
     }
 
@@ -109,4 +112,5 @@ class LastScreen : AppCompatActivity() {
     fun onClickBack() {
         this.finish()
     }
+
 }
