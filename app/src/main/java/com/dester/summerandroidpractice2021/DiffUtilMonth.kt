99203fd7388634281eb8386fdde2afbbaf49f29b
Daniil <fatikoff2002@mail.ru>
2021-07-11 15:19:20 +0500

    fix screen 2.2

---
+++ app/src/main/java/com/dester/summerandroidpractice2021/DiffUtilMonth.kt
@@ -0,0 +1,25 @@
+package com.dester.summerandroidpractice2021
+
+import androidx.recyclerview.widget.DiffUtil
+
+class DiffUtilMonth (
+        var oldList:ArrayList<InfoMonth>,
+        var newList:ArrayList<InfoMonth>
+    ) : DiffUtil.Callback() {
+
+        override fun getOldListSize(): Int {
+            return oldList.size
+        }
+
+        override fun getNewListSize(): Int {
+            return newList.size
+        }
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition].yearName ffgf`== newList[newItemPosition].yearName
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition].imageId == newList[newItemPosition].imageId
+        }
+}
