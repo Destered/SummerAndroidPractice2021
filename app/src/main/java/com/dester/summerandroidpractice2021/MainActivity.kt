b7722372009605ab2fcd9dd9d1cc08f18eeeeb02
Timur Boltachev <timur.boltachev@joy-dev.com>
2021-07-11 16:22:33 +0300

    singleton refactor

--- app/src/main/java/com/dester/summerandroidpractice2021/MainActivity.kt
+++ app/src/main/java/com/dester/summerandroidpractice2021/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.dester.summerandroidpractice2021.data.models.Events
 import com.dester.summerandroidpractice2021.data.models.Singleton
 import com.dester.summerandroidpractice2021.databinding.ActivityMainBinding
 import com.whiteelephant.monthpicker.MonthPickerDialog
@@ -17,6 +18,7 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
     var myYear = SimpleDateFormat("yyyy").format(Date()).toInt()
     lateinit var binding: ActivityMainBinding
+    lateinit var database: Events
     private val adapter = YearAdapter ({ number -> openMonthActivity(number) })
 
     private val imageIdList = listOf(
@@ -29,6 +31,7 @@ class MainActivity : AppCompatActivity() {
     private var yeartemp = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        database = Singleton.getInstance(this)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         init()
